a1ecf33d099df4da7c665d60579f50e2e050f3bf
damonlopes <damonoliniski@gmail.com>
2024-09-12 23:05:06 -0300

    Att: descrição

--- app/db/postgres.py
+++ app/db/postgres.py
@@ -1,11 +1,13 @@
-from psycopg2 import connect, DatabaseError
+from psycopg2 import connect
 import os
 
 class PostgresDB():
+    #Inicialização da classe
     def __init__(self):
         self.conn = self.new_connection()
         self.create_table()
 
+    #Estabelendo conexão com o banco de dados
     def new_connection(self):
         conn = connect(
             host = os.environ["PG_HOST"],
@@ -20,17 +22,20 @@ class PostgresDB():
         )
         return conn
     
+    #Criação da tabela de dados
     def create_table(self):
         cur = self.conn.cursor()
         cur.execute(open("app/db/schema.sql", "r").read())
         self.conn.commit()
 
+    #Inserção de dados de vôo
     def insert_flight(self, flight):
         insert_sql = f"INSERT INTO {os.environ["PG_SCHEMA"]}.flights (flight_date, flight_iata, flight_status, airline, dep_iata, dep_delay, arr_iata, arr_delay) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
         cur = self.conn.cursor()
         cur.execute(insert_sql, flight)
         self.conn.commit()
 
+    #Consulta de todos os vôos registrados no banco
     def select_flights_date(self):
         select_sql = f"SELECT * FROM {os.environ["PG_SCHEMA"]}.flights"
         array_values = []
@@ -41,5 +46,6 @@ class PostgresDB():
         result = cur.fetchall()
         return result
 
+    #Fechar conexão
     def conn_close(self):
         self.conn.close()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from flask import Flask, request, Response
 from dotenv import load_dotenv
-import requests
-import pandas
-import math
 import os
 
 from app.utils import external_requests
@@ -12,8 +9,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# Criação da tabela
 postgres.PostgresDB().conn_close()
 
+# Lista de status possíveis
 list_status = ["scheduled", "active", "landed", "cancelled", "incident", "diverted"]
 
 @app.route("/")
@@ -23,10 +22,12 @@ def home():
 @app.route("/get_flights")
 def get_info_flights():
 
+    # Gera o parâmetro com a chave da API
     params = {
         "access_key":os.environ["CHAVE_API"],
     }
 
+    # Verifica se na requisição consta o código IATA
     if not request.args.get("iata_code"):
         print(request.args.get("iata_code"))
         response = Response(
@@ -35,6 +36,7 @@ def get_info_flights():
         )
         return response
 
+    # Se requisitado, verifica se o valor é válido e gera o parâmetro com o status de vôo
     if request.args.get("status") is not None:
         if request.args.get("status") in list_status:        
             params["flight_status"] = request.args.get("status")
@@ -45,19 +47,19 @@ def get_info_flights():
             )
             return response
 
+    # Cria dois grupos de parâmetros, e insere o código IATA (um para Partidas e outro para Chegadas)
     dep_params = params.copy()
     arr_params = params.copy()
     dep_params["dep_iata"] = request.args.get("iata_code")
     arr_params["arr_iata"] = request.args.get("iata_code")
-    # voos partindo
-    all_flights = external_requests.get_all_flights(dep_params)
 
-    # voos chegando
+    # Requisições de vôos
+    all_flights = external_requests.get_all_flights(dep_params)
     all_flights += external_requests.get_all_flights(arr_params)
 
-    # salvar os vôos registrados no PSQL
-    pgclient = postgres.PostgresDB()
+    # Salva os vôos obtidos no PostgreSQL
     if all_flights:
+        pgclient = postgres.PostgresDB()
         for data in all_flights:
             flight = [
                 data["flight_date"],
@@ -70,15 +72,32 @@ def get_info_flights():
                 data["arrival"]["delay"],
             ]
             pgclient.insert_flight(flight)
+        pgclient.conn_close()
 
-    pgclient.conn_close()
-
+    # Retorna uma contagem com todos os vôos registrados no PostgreSQL
     return f"Teve {len(all_flights)} vôos registrados referentes ao aeroporto de {request.args.get("iata_code")}"
 
 @app.route("/info_flights")
 def select_flights():
-    # flight_date = request.args.get("flight_date")
+    # Coleta todos os vôos registrados no PostgreSQL
     pgclient = postgres.PostgresDB()
-    all_flights = pgclient.select_flights_date()
+    raw_flights = pgclient.select_flights_date()
+    all_flights = []
 
+    # Formata cada entrada, de acordo com o dado obtido
+    for flight in raw_flights:
+        all_flights.append(
+            {
+                "flight_date":flight[1],
+                "flight_iata":flight[2],
+                "flight_status":flight[3],
+                "airline":flight[4],
+                "dep_iata":flight[5],
+                "dep_delay":flight[6],
+                "arr_iata":flight[7],
+                "arr_delay":flight[8]
+            }
+        )
+
+    # Retorna todos os vôos registrados
     return all_flights
